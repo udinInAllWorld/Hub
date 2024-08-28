29229884e6992ca59b20d6289e7381bb75c983bd
udinInAllWorld <gamesaja650@gmail.com>
2024-08-28 22:11:57 +0800

    hub

--- Hub-Backup.lua
+++ Hub-Backup.lua
@@ -1,15 +1,5 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-    if game.PlaceId == 15025029467 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/15025029467.lua"))()
-    elseif game.PlaceId == 13127800756 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/13127800756.lua"))()
-    elseif game.PlaceId == 11827760850 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/11827760850.lua"))()	
-    elseif game.PlaceId == 12978425304 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/12978425304.lua"))()
-    elseif game.PlaceId == 15139160794 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Enzo-YTscript/scrp/main/15139160794.lua"))()
-    elseif game.PlaceId == 18629053284 then
+    if game.PlaceId == 18629053284 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629053284.lua"))()
     elseif game.PlaceId == 18629058177 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/udinInAllWorld/PrivateScript/main/18629058177.lua"))()
